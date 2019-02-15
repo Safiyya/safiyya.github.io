263b3b6c4beffbf5171cd7ded9836cf925d0e427
Safiyya <safiyya.babio@gmail.com>
2019-02-15 15:32:48 +0200

    try again

--- deploy.sh
+++ deploy.sh
@@ -11,18 +11,14 @@ cd dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
-
-git config --global user.email "safiyya.babio@gmail.com"
-git config --global user.name "Safiyya"
-
 git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:Safiyya/Safiyya.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Safiyya/safiyya.github.io.git master:gh-pages
+#git push -f git@github.com:Safiyya/safiyya.github.io.git master:gh-pages
 
 cd -
