967e2f64d62244853b6516e1313b8db8b1bfdf60
Safiyya <safiyya.babio@gmail.com>
2019-02-15 15:31:02 +0200

    deploy

---
+++ precache-manifest.3f40e587f3f72cfc990072889212e006.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "60f05a9b00e0f853eded",
+    "url": "/js/chunk-vendors.d399512c.js"
+  },
+  {
+    "revision": "18f1e8c0d646c9797948",
+    "url": "/js/app.7950a440.js"
+  },
+  {
+    "revision": "21f709b7a710cc9b88ce",
+    "url": "/js/about.85056604.js"
+  },
+  {
+    "revision": "9689ff8d7f9eb97c9a160470edea108b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "18f1e8c0d646c9797948",
+    "url": "/css/app.68fa37a6.css"
+  }
+];
