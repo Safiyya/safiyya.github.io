1cb2f9668a40a5a14ee90358c7fe3d7eafa674ba
Safiyya <safiyya.babio@gmail.com>
2019-02-22 20:32:25 +0200

    deply from master

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:Safiyya/Safiyya.github.io.git gh-pages
+git push -f git@github.com:Safiyya/Safiyya.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 #git push -f git@github.com:Safiyya/safiyya.github.io.git master:gh-pages
