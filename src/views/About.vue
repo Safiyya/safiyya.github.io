ab30f6aba3856349c78c0139e358889bc547dea4
Safiyya <safiyya.babio@gmail.com>
2019-03-24 14:39:30 +0200

    adding about section

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,22 @@
 <template>
-  <div class="bg-secondary flex flex-col items-center py-32">
-    <div class="text">
-Hi, I'm Safiyya. Nice to meet you.
-    </div>
-    <div>
-      I’m full-stack developer with a growing interest in UI/UX design. For the past 10 years, I have helped rising startups & small business owners build anything from AI chatbots, analytics platform and online learning systems.
+  <div class="bg-secondary flex flex-col items-center py-24 text-white">
+    <div class="w-3/5">
+      <h2 class="text">
+        Hi, I'm Safiyya. Nice to meet you.
+      </h2>
+      <h4 class="py-4">
+        <div class="py-3">
+          I’m full-stack developer with a growing interest in UI/UX design. For the past 10 years, I have helped rising startups & small business owners build anything from AI chatbots, analytics platform and online learning systems.
 
+        </div>
+
+        <div >
+          Remote work is second nature to me and I communicate with my clients clearly and effectively.
+
+        </div>
+      </h4>
     </div>
 
-    <div>
-      Remote work is second nature to me and I communicate with my clients clearly and effectively.
-
-    </div>
   </div>
 </template>
  
